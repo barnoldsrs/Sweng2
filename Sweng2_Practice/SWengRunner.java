bdc928c971ce8808a57fb12e3cf29dd9faa97ad8
arnoldb1778 <arnoldb1778@gmail.com>
2020-08-29 20:10:35 -0700

    4th commit

--- Sweng2_Practice/SWengRunner.java
+++ Sweng2_Practice/SWengRunner.java
@@ -12,6 +12,8 @@ public class SWengRunner
         System.out.println("Hey, this is the first line of the runner class.");
 	System.out.println("This was added by arnold1778.");
 	System.out.println("This 3rd line was added by barnoldsrs after a clone");
+	System.out.println("4th line was 2nd change by arnoldb1778");
+
 
 
     }
