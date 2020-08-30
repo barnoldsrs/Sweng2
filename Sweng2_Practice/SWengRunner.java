a59c693d542b06de14ad55303138a682d3be3c73
Brian Arnold <barnold@sageridge.org>
2020-08-29 19:07:50 -0700

    Runner baseline.

---
+++ Sweng2_Practice/SWengRunner.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class SWengRunner here.
+ *
+ * @author Brian Arnold
+ * @version Aug 29, 2020
+ */
+public class SWengRunner
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hey, this is the first line of the runner class.");
+    }
+    
+}
