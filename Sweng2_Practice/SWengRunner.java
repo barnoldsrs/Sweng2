380978d5524cef5ce7ba42c1b4c932bd653e824e
arnoldb1778 <arnoldb1778@gmail.com>
2020-08-29 20:08:05 -0700

    Added 3rd line

--- Sweng2_Practice/SWengRunner.java
+++ Sweng2_Practice/SWengRunner.java
@@ -11,6 +11,8 @@ public class SWengRunner
     {
         System.out.println("Hey, this is the first line of the runner class.");
 	System.out.println("This was added by arnold1778.");
+	System.out.println("This 3rd line was added by barnoldsrs after a clone");
+
 
     }
     
